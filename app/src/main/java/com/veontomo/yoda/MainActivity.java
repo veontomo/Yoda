1493a4d7e42fc1b8b63ff117dcc1b7eb703bdf73
Mario Rossi <veontomo@gmail.com>
2016-04-11 12:42:34 +0200

    feat: change the app icon

--- app/src/main/java/com/veontomo/yoda/MainActivity.java
+++ app/src/main/java/com/veontomo/yoda/MainActivity.java
@@ -3,11 +3,22 @@ package com.veontomo.yoda;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void loadText(String text) {
+    }
+
+    @Override
+    public void loadList(List<String> sayings) {
+
     }
 }
