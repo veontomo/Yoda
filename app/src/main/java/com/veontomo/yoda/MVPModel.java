6031936ff068ae3890c8c21399f0b7848da6f19c
Mario Rossi <veontomo@gmail.com>
2016-04-12 15:32:38 +0200

    feat: create a subscriber for recieving the user input

--- app/src/main/java/com/veontomo/yoda/MVPModel.java
+++ app/src/main/java/com/veontomo/yoda/MVPModel.java
@@ -2,10 +2,35 @@ package com.veontomo.yoda;
 
 import android.util.Log;
 
+import rx.Observable;
+import rx.Subscriber;
+
 /**
  * Model class of the activity according to MVP approach
  */
 public class MVPModel {
+    private final Subscriber<String> mUserInputReceiver;
+
+    public MVPModel(){
+
+        mUserInputReceiver = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.i(Config.appName, "is over");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(Config.appName, "an error occurred: " + e.getMessage());
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.i(Config.appName, "enqueued for translation: "  + s);
+            }
+        };
+    }
 
     /**
      * This method gets called once the presenter receives a text to translate.
@@ -14,6 +39,7 @@ public class MVPModel {
      */
     public void onTranslate(String text, final MVPPresenter presenter) {
         Log.i(Config.appName, "text: " + text + " is received.");
+        Observable.just(text).subscribe(mUserInputReceiver);
         onTranslated("translated what Yoda said" + text, presenter);
     }
 
