62830b36d46c3357bd2aae90ad534e203f2d8b90
Mario Rossi <veontomo@gmail.com>
2016-04-13 19:47:47 +0200

    feat: stub quote-related API

---
+++ app/src/main/java/com/veontomo/yoda/QuotesApi.java
@@ -0,0 +1,18 @@
+package com.veontomo.yoda;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+/**
+ * API for quote retrieval
+ *
+ */
+public interface QuotesApi {
+    @Headers({
+            "Accept: application/json",
+            "X-Mashape-Key: " + Config.YODA_API_KEY})
+    @GET("")
+    Call<String> getByCategory(@Query("cat") String category);
+}
