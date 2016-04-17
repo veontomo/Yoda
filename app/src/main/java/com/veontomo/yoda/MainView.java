910160343c8fe6c0135d46b8f00c26e69de6e27e
Mario Rossi <veontomo@gmail.com>
2016-04-17 07:09:37 +0200

    feat: add call log

--- app/src/main/java/com/veontomo/yoda/MainView.java
+++ app/src/main/java/com/veontomo/yoda/MainView.java
@@ -1,6 +1,10 @@
 package com.veontomo.yoda;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.provider.CallLog;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
+import java.util.Date;
 import java.util.List;
 
 import rx.Observer;
@@ -29,8 +34,58 @@ public class MainView extends AppCompatActivity implements MVPView {
         setContentView(R.layout.activity_main);
 
         consequtiveStreams();
+        calllog();
 
+
+    }
+
+    private void calllog() {
         Log.i(Config.appName, "last call: " + CallLog.Calls.getLastOutgoingCall(this));
+        StringBuffer stringBuffer = new StringBuffer();
+        //if (checkPermission())
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.i(Config.appName, "no permissions");
+            return;
+        }
+        Cursor cursor = getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                null, null, null, CallLog.Calls.DATE + " DESC");
+        int number = cursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = cursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);
+        while (cursor.moveToNext()) {
+            String phNumber = cursor.getString(number);
+            String callType = cursor.getString(type);
+            String callDate = cursor.getString(date);
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String callDuration = cursor.getString(duration);
+            String dir = null;
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+            stringBuffer.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
+                    + dir + " \nCall Date:--- " + callDayTime
+                    + " \nCall duration in sec :--- " + callDuration);
+            stringBuffer.append("\n----------------------------------");
+        }
+        cursor.close();
     }
 
 
