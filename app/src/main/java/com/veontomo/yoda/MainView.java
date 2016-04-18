c53b42653e44268a9d9032b97cc28f8fa1a371ba
Mario Rossi <veontomo@gmail.com>
2016-04-18 07:59:30 +0200

    refactor: remove call-related functionality

--- app/src/main/java/com/veontomo/yoda/MainView.java
+++ app/src/main/java/com/veontomo/yoda/MainView.java
@@ -1,12 +1,7 @@
 package com.veontomo.yoda;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.provider.CallLog;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
@@ -14,7 +9,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
-import java.util.Date;
 import java.util.List;
 
 import rx.Observer;
@@ -34,71 +28,21 @@ public class MainView extends AppCompatActivity implements MVPView {
         setContentView(R.layout.activity_main);
 
         consequtiveStreams();
-        calllog();
 
 
-    }
-
-    private void calllog() {
-        Log.i(Config.appName, "last call: " + CallLog.Calls.getLastOutgoingCall(this));
-        StringBuffer stringBuffer = new StringBuffer();
-        //if (checkPermission())
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.i(Config.appName, "no permissions");
-            return;
-        }
-        Cursor cursor = getContentResolver().query(CallLog.Calls.CONTENT_URI,
-                null, null, null, CallLog.Calls.DATE + " DESC");
-        int number = cursor.getColumnIndex(CallLog.Calls.NUMBER);
-        int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
-        int date = cursor.getColumnIndex(CallLog.Calls.DATE);
-        int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);
-        while (cursor.moveToNext()) {
-            String phNumber = cursor.getString(number);
-            String callType = cursor.getString(type);
-            String callDate = cursor.getString(date);
-            Date callDayTime = new Date(Long.valueOf(callDate));
-            String callDuration = cursor.getString(duration);
-            String dir = null;
-            int dircode = Integer.parseInt(callType);
-            switch (dircode) {
-                case CallLog.Calls.OUTGOING_TYPE:
-                    dir = "OUTGOING";
-                    break;
-                case CallLog.Calls.INCOMING_TYPE:
-                    dir = "INCOMING";
-                    break;
-
-                case CallLog.Calls.MISSED_TYPE:
-                    dir = "MISSED";
-                    break;
-            }
-            stringBuffer.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
-                    + dir + " \nCall Date:--- " + callDayTime
-                    + " \nCall duration in sec :--- " + callDuration);
-            stringBuffer.append("\n----------------------------------");
-        }
-        cursor.close();
     }
 
 
     @Override
-    public void onStart(){
+    public void onStart() {
         super.onStart();
         init();
     }
 
-    public void start(View view){
+    public void start(View view) {
         //mQuoteText.setText("aaaaaaaaaaaaaaaa");
         // switcher.showPrevious();
-        if (mPresenter != null){
+        if (mPresenter != null) {
             mPresenter.onStart();
         }
     }
@@ -116,7 +60,7 @@ public class MainView extends AppCompatActivity implements MVPView {
 
     @Override
     public void loadText(String text) {
-        if (mTranslation != null){
+        if (mTranslation != null) {
             mTranslation.setText(text);
         } else {
             Log.i(Config.appName, "Can not load");
@@ -142,7 +86,6 @@ public class MainView extends AppCompatActivity implements MVPView {
     }
 
 
-
     public void textViewClicked(View v) {
         switcher.showNext();
     }
@@ -153,15 +96,15 @@ public class MainView extends AppCompatActivity implements MVPView {
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.radio_movie:
                 if (checked)
                     mPresenter.setCategory(MVPModel.CATEGORY_MOVIES);
-                    break;
+                break;
             case R.id.radio_famous:
                 if (checked)
                     mPresenter.setCategory(MVPModel.CATEGORY_FAMOUS);
-                    break;
+                break;
         }
     }
 
@@ -193,7 +136,6 @@ public class MainView extends AppCompatActivity implements MVPView {
         subject.subscribeOn(Schedulers.io()).subscribe(greeter);
         subject.onNext("hi!");
         subject.onNext("bye!");
-
 
 
     }
