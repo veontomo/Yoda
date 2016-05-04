295de5c93c168f364d73c0fb0762cea7f9bf28e4
Mario Rossi <veontomo@gmail.com>
2016-05-04 11:16:54 +0200

    feat: store quotes in a cache
    Create a cache of quotes, and in case a quote can not be retrieved, pick
    up a random one from the cache.

--- app/src/main/java/com/veontomo/yoda/MVPRetrieveModel.java
+++ app/src/main/java/com/veontomo/yoda/MVPRetrieveModel.java
@@ -2,6 +2,10 @@ package com.veontomo.yoda;
 
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -22,6 +26,13 @@ public class MVPRetrieveModel {
 
     private final Callback<Quote> mQuoteCallback;
 
+    private final List<Quote> cache;
+
+    /**
+     * max number of the elements that the cache might contain
+     */
+    private final int maxCacheSize = 10;
+
     /**
      * Category of the quote.
      * By default it is set to "movies".
@@ -37,6 +48,9 @@ public class MVPRetrieveModel {
      * Constructor.
      */
     public MVPRetrieveModel() {
+
+        cache = new ArrayList<>();
+
         Retrofit retrofit2 = new Retrofit.Builder()
                 .baseUrl(Config.QUOTES_SERVICE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -48,17 +62,53 @@ public class MVPRetrieveModel {
             public void onResponse(Call<Quote> call, Response<Quote> response) {
                 if (response.isSuccessful()) {
                     onQuoteReceived(response.body());
+                    store(response.body());
                 } else {
-                    onRetrieveFailure("Something wrong I have retrieved.");
+                    if (!cache.isEmpty()) {
+                        onQuoteReceived(getRandomFromCache());
+                    } else {
+                        onRetrieveFailure("Something wrong I have retrieved.");
+                    }
                 }
             }
 
             @Override
             public void onFailure(Call<Quote> call, Throwable t) {
-                onRetrieveFailure(t.getMessage());
+                if (!cache.isEmpty()) {
+                    onQuoteReceived(getRandomFromCache());
+                } else {
+                    onRetrieveFailure(t.getMessage());
+                }
             }
         };
 
+    }
+
+    /**
+     * Returns a random quote from the cache.
+     *
+     * @return
+     */
+    private Quote getRandomFromCache() {
+        Random generator = new Random();
+        int pos = generator.nextInt(cache.size());
+        Log.i(TAG, "getRandomFromCache: returning quote n." + pos + " from cache of size " + cache.size() );
+        return cache.get(pos);
+    }
+
+    /**
+     * Stores the quote in the cache.
+     * <p/>
+     * If the cache becomes bigger than specified limit, the first element of the cache is removed.
+     *
+     * @param quote quote to put in the cache
+     */
+    private void store(Quote quote) {
+        Log.i(TAG, "store: add quote to the cache");
+        if (cache.size() >= maxCacheSize) {
+            cache.remove(0);
+        }
+        cache.add(quote);
     }
 
     private void onRetrieveFailure(final String s) {
